c978122270109c62834d547297058b7db1fb97b6
Elliott Kasoar <elliott.kasoar@btinternet.com>
2020-04-18 15:23:39 +0100

    Comments

--- trees.py
+++ trees.py
@@ -8,15 +8,18 @@ Created on Fri Apr 17 21:35:10 2020
 
 # Node structure to create binary trees
 class Node:
-    
+
+    # Define of nodes - data, left and right children    
     def __init__(self, data):
         self.data = data
         self.left = None
         self.right = None
 
+    # Print data contained in a given node
     def print_node(self):
         print(self.data)
 
+    # Return height of node (maximum level in tree)
     def height(self, level, max_level):
         
         level += 1
@@ -31,7 +34,7 @@ class Node:
           
         return max_level
 
-
+    # Return nodes in depth-first order - descend fully down each branch first
     def deep_list(self, level, lst):
         
         level += 1
@@ -47,6 +50,8 @@ class Node:
         
         return lst
 
+    # Return nodes in breath-first order - each level at a time, left to right
+    # Doesn't currently work
     def breadth_list(self, level, lst):
         
         level += 1
@@ -62,21 +67,25 @@ class Node:
         
         return lst        
 
-        
+
+# Create some nodes for tree
 root = Node(1)
 root.left = Node(2)
 root.right = Node(3)
 root.left.left = Node(4)
 root.left.right = Node(5) 
 
+#Print various nodes
 # root.print_node() # Equivalent to print(root.data)
 # root.left.print_node()
 # root.right.print_node()
 # root.left.left.print_node()
 # root.left.right.print_node()
 
+# Print whether nodes exist - useful expressions used
 # print(root.right.right is None)
 # print(root.left.right is None)
 
 print("Deep list = ", root.deep_list(0, []))
+print("Breadth list = ", root.breadth_list(0, []))
 print("Tree height = ", root.height(0, 0))

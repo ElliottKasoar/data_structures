adfb5e52d4af0346d075e744cf1032efc0335bbf
Elliott Kasoar <elliott.kasoar@btinternet.com>
2020-04-18 01:16:24 +0100

    Basic tree via class

--- trees.py
+++ trees.py
@@ -7,3 +7,26 @@ Created on Fri Apr 17 21:35:10 2020
 """
 
 # Trees
+class Node:
+    def __init__(self, data):
+        self.left = None
+        self.right = None
+        self.data = data
+
+    def PrintTree(self):
+        print(self.data)
+
+
+root = Node(1)
+# root.PrintTree()
+root.left = Node(2)
+# root.PrintTree()
+root.right = Node(3)
+# root.PrintTree()
+root.left.left = Node(4)
+
+print(root.data)
+print(root.left.data)
+print(root.right.data)
+print(root.left.left.data)
+print(root.left.right.data)

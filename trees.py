bd5a7422433eb7b92e85a6badbfa30fb54c06e1d
Elliott Kasoar <elliott.kasoar@btinternet.com>
2020-04-22 03:23:19 +0100

    Breadth search function

--- trees.py
+++ trees.py
@@ -15,12 +15,14 @@ class Node:
         self.left = None
         self.right = None
 
+
     # Print data contained in a given node
     def print_node(self):
         print(self.data)
 
+
     # Return height of node (maximum level in tree)
-    def height(self, level, max_level):
+    def height(self, level=0, max_level=0):
         
         level += 1
         if level > max_level:
@@ -34,8 +36,9 @@ class Node:
           
         return max_level
 
+
     # Return nodes in depth-first order - descend fully down each branch first
-    def deep_list(self, level, lst):
+    def deep_list(self, level=0, lst=[]):
         
         level += 1
         
@@ -50,22 +53,44 @@ class Node:
         
         return lst
 
+
     # Return nodes in breath-first order - each level at a time, left to right
     # Doesn't currently work
-    def breadth_list(self, level, lst):
+    # Want self, then self.left, self.right, then
+    # self.left.left, self.left.right, self.right.left, self.right.right
+    # etc...
+    def breadth_list(self, current_level=0, max_level=1, lst=[], cont=False):
+
+        # print(max_level)        
+
+        current_level += 1
+
+        if ((current_level == 1) and max_level == 1):
+            lst.append(self.data)
+            max_level += 1
         
-        level += 1
-        
-        print("Level = ", level, "data = ", self.data)
-        lst.append(self.data)
-        
-        if (self.left is not None):
-            self.left.breadth_list(level, lst)
+        if (current_level < max_level):
             
-        if (self.right is not None):
-            self.right.breadth_list(level, lst)
+            if (self.left is not None):
+                lst, cont = self.left.breadth_list(current_level, max_level, lst, cont)
+            
+            if (self.right is not None):
+                lst, cont = self.right.breadth_list(current_level, max_level, lst, cont)
         
-        return lst        
+            if (cont):
+                max_level += 1
+                self.breadth_list(max_level=max_level, lst=lst)
+        
+        if (current_level == max_level):
+
+            lst.append(self.data)
+            # print("Level = ", current_level, "data = ", self.data)
+
+            if ((self.left is not None) or (self.right is not None)):
+                # print("TEST")
+                cont = True
+        
+        return lst, cont
 
 
 # Create some nodes for tree
@@ -74,6 +99,8 @@ root.left = Node(2)
 root.right = Node(3)
 root.left.left = Node(4)
 root.left.right = Node(5) 
+root.right.left = Node(6)
+root.right.right = Node(7)
 
 #Print various nodes
 # root.print_node() # Equivalent to print(root.data)
@@ -86,6 +113,9 @@ root.left.right = Node(5)
 # print(root.right.right is None)
 # print(root.left.right is None)
 
-print("Deep list = ", root.deep_list(0, []))
-print("Breadth list = ", root.breadth_list(0, []))
-print("Tree height = ", root.height(0, 0))
+print("Deep list = ", root.deep_list())
+
+lst, cont = root.breadth_list()
+print("Breadth list = ", lst)
+
+print("Tree height = ", root.height())
